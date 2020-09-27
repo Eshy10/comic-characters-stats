bbf642a4d4c4098e5aadf5f9bf32dbdf8ac55f88
Eshy10 <adewalebose15@gmail.com>
2020-09-27 21:01:09 +0100

    add api functions

---
+++ src/api/fetchCharacterApi.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const HeroCharacter = (() => {
+const getAllCharacter = () => {
+   const promise = axios.get('https://akabab.github.io/superhero-api/api/all.json')
+    const promiseData = promise.then(res => res.data)
+    promise.catch(error => console.log(error))
+    return promiseData;
+}
+const getCharacterId = (id) => {
+    const promise = axios.get(`https://akabab.github.io/superhero-api/api/id/${id}.json`)
+    const promiseData = promise.then(res => res.data)
+    promise.catch(error => console.log(error))
+    return promiseData;
+}
+return { getAllCharacter, getCharacterId }
+})()
+
+export default HeroCharacter;
