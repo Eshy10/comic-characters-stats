3d849ddb123db096e443f5e8547e9a3d17b70f57
Eshy10 <adewalebose15@gmail.com>
2020-09-27 21:05:14 +0100

    set up redux store

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducer/rootReducer';
+import thunk from 'redux-thunk';
+
+const middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(thunk);
+}
+
+const initialState = {
+  heroes: [],
+  filter: '',
+  character: {}
+};
+
+const store = createStore(rootReducer, initialState, applyMiddleware(...middlewares));
+
+export default store;
