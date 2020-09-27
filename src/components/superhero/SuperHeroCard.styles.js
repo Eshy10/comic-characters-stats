732911264c915b1d044f4c251c786d3a8f8a7571
Eshy10 <adewalebose15@gmail.com>
2020-09-27 21:16:37 +0100

    add super hero card and styles

---
+++ src/components/superhero/SuperHeroCard.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 300,
+    },
+  });
+
+  export { useStyles };
