b5c083f39c26dfb16291e89ec02dde33d3006be4
Eshy10 <adewalebose15@gmail.com>
2020-09-27 21:10:06 +0100

    add navbar component

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import {useStyles} from './Navbar.styles';
+
+
+const Navbar = ({props, handleChange}) => {
+    const classes = useStyles(props)
+    return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            MyComic
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+              onChange = {handleChange}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+)}
+
+export default Navbar;
